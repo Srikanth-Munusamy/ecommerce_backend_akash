3a9a7a4cb0b1559cb0caac0d70db20cca3c7c32b
Akash R <rvrakash2003@gmail.com>
2024-08-22 21:50:20 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const { Sequelize } = require("sequelize");
 
-const DB_NAME = process.env.DB_NAME || 'karthike_ecommerce_backend';
-const DB_USERNAME = process.env.DB_USERNAME || 'karthike_ecommerce_backend';
+const DB_NAME = process.env.DB_NAME;
+const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 3306;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
